7612917ed76c72c242e690e70938fbe3dbe11fe0
Santo Shakil <shakil.mezbah@gmail.com>
2025-07-06 14:48:55 +0600

    cleanup: remove duplicate FFI implementation
    - Remove old lightning-db-ffi directory (with dash)
    - Keep only the new complete lightning_db_ffi implementation (with underscore)
    - Update workspace to remove old FFI crate
    - This eliminates confusion from having two FFI directories

--- lightning-db-ffi/dart_bindings/lib/lightning_db.dart
+++
@@ -1,7 +0,0 @@
-/// Lightning DB - High-performance embedded database for Dart
-library lightning_db;
-
-export 'src/database.dart' show LightningDB, LightningDBConfig;
-export 'src/errors.dart';
-
-// Note: Do not export generated bindings as they are internal implementation details
